0e62fa968697c6fb1b6aa414bc156467a870ae7f
Ismael Gbian <ismael.gbian@gmail.com>
2021-01-21 17:13:40 -0500

    Added a size component

---
+++ Assets/Components/SizeComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct SizeComponent : IComponent
+{
+    public SizeComponent(float size)
+    {
+        this.size = size;
+    }
+    public float size;
+}
