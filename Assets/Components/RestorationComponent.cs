3646b43c8c9b45faef65a2fe321b4856fa5b35e7
Ismael Gbian <ismael.gbian@gmail.com>
2021-01-28 06:57:22 -0500

    Added a restoration component

---
+++ Assets/Components/RestorationComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestorationComponent : IComponent
+{
+    public RestorationComponent()
+    {
+        this.coolDownStartTime = -1;
+        this.restorationRequired = false;
+    }
+
+    public float coolDownStartTime;
+    public bool restorationRequired;
+}
